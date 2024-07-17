986340fe07c5f78df0feb3dc77024bb0fdc4ca19
Dewansh Pandey <dewansh1503@gmail.com>
2024-07-17 10:00:17 +0530

    connect to mongodb atlas

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDb = async function () {
+   try {
+      const connectionInstance = await mongoose.connect(
+         `${process.env.MONGODB_URI}/${DB_NAME}`
+      );
+      console.log("Database connected !!");
+      console.log("DB_Host is:", connectionInstance.connection.host);
+   } catch (error) {
+      console.log("MonogDB connection failed:", error);
+      process.exitCode = 1;
+   }
+};
+export default connectDb;
+// export { connectDb as default };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log("running fine");
+import connectDb from "./db/index.js";
+
+let res = await connectDb();
+// console.log(res);
+
+/* This way we can connect to the database 
+// import dotenv from "dotenv"; //not need to import process it is already available in node
+import mongoose from "mongoose";
+import express from "express";
+import { DB_NAME } from "./constants.js";
+
+const app = express();
+// console.log(process.env.MONGODB_URI, DB_NAME);
+(async () => {
+   try {
+      const res = await mongoose.connect(
+         `${process.env.MONGODB_URI}/${DB_NAME}`
+      );
+        app.on("err", (error) => {
+           console.log('error hai');
+        });
+      app.listen(process.env.PORT, () => {
+         console.log("hey i am listening on 8000");
+      });app.listen()
+   } catch (error) {
+      console.log("ERROR is ");
+        throw error;
+   }
+})();
+ */
