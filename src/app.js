369b6a4643d696ecda57c684a104a555e5dc719d
Dewansh Pandey <dewansh1503@gmail.com>
2024-07-24 00:00:03 +0530

    add middlewares

--- src/app.js
+++ src/app.js
@@ -1,5 +1,23 @@
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import express from "express";
+
 const app = express();
+
+//enabling all cors request
+app.use(
+   cors({
+      origin: process.env.CORS_ORIGIN,
+      credentials: true,
+   })
+);
+//getting request in form of json data and setting its limit
+app.use(express.json({ limit: "16kb" }));
+
+//getting data from the URL (extended means object inside object)
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+
 
 //setting the event listener on ready and errr(i.e. error)
 app.on("errr", () => {
