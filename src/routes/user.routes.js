5b6eedba992ab17a1546f9f8966725768165e2ff
Dewansh Pandey <dewansh1503@gmail.com>
2024-08-04 17:58:22 +0530

    add regiter controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,22 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+   upload.fields([
+      // we cannot use upload.array() because it takes multiple args(files) in one sigle field only
+      {
+         name: "avatar",
+         maxCount: 1,
+      },
+      {
+         name: "coverImage",
+         maxCount: 1,
+      },
+   ]),
+   registerUser
+);
 
 export default router;
