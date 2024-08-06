355bff426a103d10a903863726ec7da571039646
Dewansh Pandey <dewansh1503@gmail.com>
2024-08-06 18:02:52 +0530

    Bug fixes

--- src/db/index.js
+++ src/db/index.js
@@ -7,7 +7,7 @@ const connectDb = async function () {
          `${process.env.MONGODB_URI}/${DB_NAME}`
       );
       console.log("Database connected !!");
-      console.log("DB_Host is:", connectionInstance.connection.host);
+      // console.log("DB_Host is:", connectionInstance.connection.host);
    } catch (error) {
       console.log("MonogDB connection failed:", error);
       process.exitCode = 1;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-dotenv.config({ path: "./env" });
+dotenv.config({ path: "./.env" });
 import connectDb from "./db/index.js";
 import { app } from "./app.js";
 
