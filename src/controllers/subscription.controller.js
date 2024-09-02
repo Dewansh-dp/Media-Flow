fb1cf28dce0348ee817fd30fb2c50378a1f0acd4
Dewansh Pandey <dewansh1503@gmail.com>
2024-09-02 19:06:26 +0530

    add subscribe/unsubscribe controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,9 +1,33 @@
-import { asyncHandler } from "../utils/asyncHandler";
+import { Subscription } from "../models/subscription.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
+   const { channelId } = req.params;
 
-    
+   // findOneAndDelete returns deleted document or null if document not found
+   const channelDocument = await Subscription.findOneAndDelete({
+      $and: [{ channel: channelId }, { subscriber: req.user._id }],
+   });
 
+   if (channelDocument) {
+      res.status(200).json(
+         new ApiResponse(
+            200,
+            { Unsubscribed: true },
+            "Unsubscribed successfully"
+         )
+      );
+   } else {
+      await Subscription.create({
+         channel: channelId,
+         subscriber: req.user._id,
+      });
+
+      res.status(200).json(
+         new ApiResponse(200, { subscribed: true }, "Subscribed successfully")
+      );
+   }
 });
 
 export { toggleSubscription };
