a5b9d20496e94225282a9427e8be984af2a9652b
Dewansh Pandey <dewansh1503@gmail.com>
2024-09-04 17:52:22 +0530

    add comments

--- src/middlewares/video_id.middleware.js
+++ src/middlewares/video_id.middleware.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { v2 as cloudinary } from "cloudinary";
 import { Video } from "../models/video.model.js";
 
-// this middleware fetch the video document _id
+// this middleware fetch the video document _id from the cloudinaryPublicId
 const getVideoId = asyncHandler(async (req, _, next) => {
    const { cloudinaryPublicId } = req.body;
    const { secure_url } = await cloudinary.api.resource(
