c80645b682b7bbfb3ffad469f7f8ac2ed323208f
Dewansh Pandey <dewansh1503@gmail.com>
2024-09-04 14:32:37 +0530

    add get videoId middleware

---
+++ src/middlewares/video_id.middleware.js
@@ -0,0 +1,17 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { v2 as cloudinary } from "cloudinary";
+import { Video } from "../models/video.model.js";
+
+// this middleware fetch the video document _id
+const getVideoId = asyncHandler(async (req, _, next) => {
+   const { cloudinaryPublicId } = req.body;
+   const { secure_url } = await cloudinary.api.resource(
+      `Youtube/Videos/${cloudinaryPublicId}`,
+      { resource_type: "video" }
+   );
+   const videoDocument = await Video.findOne({ videoFile: secure_url });
+   req.videoId = videoDocument._id;
+   next();
+});
+
+export { getVideoId };
