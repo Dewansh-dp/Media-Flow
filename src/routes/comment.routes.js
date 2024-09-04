f5ad7f458fb7f2bed04c0b36413bb5039529bffb
Dewansh Pandey <dewansh1503@gmail.com>
2024-09-04 17:51:59 +0530

    add routes for updating comment

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { addComment } from "../controllers/comment.controller.js";
+import { addComment, updateComment } from "../controllers/comment.controller.js";
 import { getVideoId } from "../middlewares/video_id.middleware.js";
 
 const router = Router();
@@ -8,5 +8,7 @@ const router = Router();
 router.use(verifyJWT);
 
 router.route("/comment").post(getVideoId, addComment);
+
+router.route("/update-comment").patch(getVideoId,updateComment)
 
 export default router;
