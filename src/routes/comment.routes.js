3b6d6fc2f9c1e814ce8532ddfb51579f06a7fbe4
Dewansh Pandey <dewansh1503@gmail.com>
2024-09-04 15:44:50 +0530

    add routes for comment

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { verifyJWT } from "../middlewares/auth.middleware";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { addComment } from "../controllers/comment.controller.js";
+import { getVideoId } from "../middlewares/video_id.middleware.js";
 
 const router = Router();
 
 router.use(verifyJWT);
 
-router.route("/");
+router.route("/comment").post(getVideoId, addComment);
 
 export default router;
