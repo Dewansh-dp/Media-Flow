0fd5c66dc756a140ba3c3e086d75f267b3082411
Dewansh Pandey <dewansh1503@gmail.com>
2024-09-04 08:12:43 +0530

    add routes for get subscribers

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { toggleSubscription } from "../controllers/subscription.controller.js";
+import {
+   getUserChannelSubscribers,
+   toggleSubscription,
+} from "../controllers/subscription.controller.js";
 
 const router = Router();
 
 router.use(verifyJWT);
-router.route("/toggle-subscription/:channelId").post(toggleSubscription)
 
+router.route("/toggle-subscription/:channelId").post(toggleSubscription);
+
+router.route("/subscribers/:channelId").get(getUserChannelSubscribers);
 export default router;
