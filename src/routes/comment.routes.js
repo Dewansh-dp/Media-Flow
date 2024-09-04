3bd27a6319903b18e63cdc976b13c9e72f3db48e
Dewansh Pandey <dewansh1503@gmail.com>
2024-09-04 18:30:37 +0530

    add routes for delete comment

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { addComment, updateComment } from "../controllers/comment.controller.js";
+import { addComment, deleteComment, updateComment } from "../controllers/comment.controller.js";
 import { getVideoId } from "../middlewares/video_id.middleware.js";
 
 const router = Router();
@@ -10,5 +10,7 @@ router.use(verifyJWT);
 router.route("/comment").post(getVideoId, addComment);
 
 router.route("/update-comment").patch(getVideoId,updateComment)
+
+router.route("/delete-comment").delete(getVideoId,deleteComment)
 
 export default router;
