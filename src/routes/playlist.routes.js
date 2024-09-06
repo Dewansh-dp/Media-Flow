33c64779a34b4499a061f62729fcb6d5b923ddc5
Dewansh Pandey <dewansh1503@gmail.com>
2024-09-06 17:11:34 +0530

    add route for delete video from playlist controller

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -4,6 +4,7 @@ import {
    createPlaylist,
    getPlaylistById,
    getUserPlaylists,
+   removeVideoFromPlaylist,
 } from "../controllers/playlist.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -17,5 +18,7 @@ router.route("/get-all-playlists").get(getUserPlaylists);
 router.route("/get-playlist/:playlistId").get(getPlaylistById);
 
 router.route("/add-video-to-playlist/:playlistId/:videoId").patch(addVideoToPlaylist);
+
+router.route("/remove-video-from-playlist/:playlistId/:videoId").patch(removeVideoFromPlaylist)
 
 export default router;
