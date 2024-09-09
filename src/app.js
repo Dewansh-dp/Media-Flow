6e4e8ac7236b3428adbaa25272e78021082f9b11
Dewansh Pandey <dewansh1503@gmail.com>
2024-09-09 09:09:46 +0530

    add app changes

--- src/app.js
+++ src/app.js
@@ -45,6 +45,7 @@ import videoRouter from "./routes/video.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
 
 // routes declaration
 
@@ -55,8 +56,10 @@ app.use("/api/v1/videos", videoRouter);
 
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
-app.use("/api/v1/comments",commentRouter)
+app.use("/api/v1/comments", commentRouter);
 
-app.use("/api/v1/playlists",playlistRouter)
+app.use("/api/v1/playlists", playlistRouter);
+
+app.use("/api/v1/tweets", tweetRouter);
 
 export { app };
