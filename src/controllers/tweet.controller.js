7b303409aded3e3d7b251dd137628db9d5d602ef
Dewansh Pandey <dewansh1503@gmail.com>
2024-09-09 09:23:11 +0530

    add get tweet controller

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -9,13 +9,23 @@ const createTweet = asyncHandler(async (req, res) => {
       throw new ApiError(400, "No content provided");
    }
 
-   const tweet =await Tweet.create({
+   const tweet = await Tweet.create({
       content,
       owner: req.user._id,
    });
 
-   console.log(tweet);
    res.status(200).json(new ApiResponse(200, tweet, "Tweeted successfully"));
 });
 
-export { createTweet };
+const getUserTweets = asyncHandler(async (req, res) => {
+   const tweets = await Tweet.find({ owner: req.user._id }).select("owner content updatedAt")
+   if (!tweets.length) {
+      throw new ApiError(400, "No tweets found");
+   }
+
+   res.status(200).json(
+      new ApiResponse(200, tweets, "Fetched tweets successfully")
+   );
+});
+
+export { createTweet, getUserTweets };
