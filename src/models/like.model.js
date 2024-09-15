001e4fd3c3bd3373413d25c95721b7fa0e6f3a07
Dewansh Pandey <dewansh1503@gmail.com>
2024-09-15 12:52:59 +0530

    add like model

--- src/models/like.model.js
+++ src/models/like.model.js
@@ -1,1 +1,13 @@
-ÿþ
+import mongoose, { Schema } from "mongoose";
+
+const likeSchema = new Schema(
+   {
+      video: { type: mongoose.Types.ObjectId, ref: "Video" },
+      comment: { type: mongoose.Types.ObjectId, ref: "Comment" },
+      tweet: { type: mongoose.Types.ObjectId, ref: "Tweet" },
+      likedBy: { type: mongoose.Types.ObjectId, ref: "User" },
+   },
+   { timestamps: true }
+);
+
+export const Like = mongoose.model("Like", likeSchema);
